51bce30de5e9619e26d94223b595b0fca0ccc87c
Anushruth Reddy <98297028+anushruth98@users.noreply.github.com>
2023-05-14 12:28:27 +0530

    Add files via upload

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+$servername = 'localhost';
+$user = 'root';
+$pass = '';
+$dbname = 'banks';
+
+$conn = mysqli_connect($servername, $user, $pass, $dbname);
+
+if (!$conn) {
+    die("Could Not Connect to the database" . mysqli_connect_error());
+}
